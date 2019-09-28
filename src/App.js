ad2ae3b11de6f7577c952d188f27f2b73318a7f7
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-09-28 18:41:41 +0300

    First Comment

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navi from './Navi';
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
+
+
+
+function App() {
+  return (
+    <div>
+      <Navi/>
+ <CategoryList/>
+ <ProductList/>
+    
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CategoryList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CategoryList extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Mehmet</h2>
+            </div>
+        )
+    }
+}
